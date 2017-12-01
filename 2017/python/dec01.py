de0ea88172b1aab348429a2e857949cd4177eaa3
Per Lejontand <per.lejontand@gmail.com>
2017-12-01 23:01:30 +0100

    Clean up a bit, use enumerate in both tasks, cut intermediate lists

--- 2017/python/dec01.py
+++ 2017/python/dec01.py
@@ -1,24 +1,21 @@
 import task
+import pprint
 
 class dec01_1(task.task):
     def run(self, data):
         sum = 0
-        ld = data[-1]
         if len(data) > 1:
-            for d in data:
-                if ld == d:
+            for idx, d in enumerate(data):
+                if data[idx - 1] == d:
                     sum = sum + int(d)
-                ld = d
         return str(sum)
 
 class dec01_2(task.task):
     def run(self, data):
         sum = 0
-        l = len(data)
-        d0 = data[:int(l / 2)]
-        d1 = data[int(l / 2):]
-        for idx, d in enumerate(d0):
-            if d == d1[idx]:
+        half = int(len(data) / 2)
+        for idx, d in enumerate(data[:half]):
+            if d == data[half + idx]:
                 sum = sum + int(d) * 2
         return str(sum)
 
