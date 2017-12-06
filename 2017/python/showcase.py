9298beb78f1420865d3b158bc80a3603543a07fa
Per Lejontand <per.lejontand@gmail.com>
2017-12-06 07:24:13 +0100

    Dec 06 tasks ez. Stupid IOS 11 alarm preventing me from better position.

--- 2017/python/showcase.py
+++ 2017/python/showcase.py
@@ -5,6 +5,7 @@ from dec02 import dec02_1, dec02_2
 from dec03 import dec03_1, dec03_2
 from dec04 import dec04_1, dec04_2
 from dec05 import dec05_1, dec05_2
+from dec06 import dec06_1, dec06_2
 
 dec01_1().runtest('challenge', echo=True)
 dec01_2().runtest('challenge', echo=True)
@@ -16,3 +17,5 @@ dec04_1().runtest('challenge', echo=True)
 dec04_2().runtest('challenge', echo=True)
 dec05_1().runtest('challenge', echo=True)
 dec05_2().runtest('challenge', echo=True)
+dec06_1().runtest('challenge', echo=True)
+dec06_2().runtest('challenge', echo=True)
