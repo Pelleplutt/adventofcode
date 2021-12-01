7784e760b86e7e52503fcacf0a8b9bdeb638415e
Per Lejontand <per.lejontand@gmail.com>
2021-12-01 20:02:13 +0100

    Add 2021 skeleton

---
+++ 2021/python/skel.py
@@ -0,0 +1,26 @@
+import task
+
+class decXX_1(task.IntTask):
+#class decXX_1(task.StrTask):
+    """
+    """
+    def run(self, line):
+        pass
+
+    def run_list(self, data):
+        pass
+
+class decXX_2(task.IntTask):
+#class decXX_2(task.StrTask):
+    """
+    """
+    def run(self, line):
+        pass
+
+    def run_list(self, data):
+        pass
+
+
+if __name__ == "__main__":
+    decXX_1().runtests()
+    decXX_2().runtests()
