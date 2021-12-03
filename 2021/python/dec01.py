b90596059ac4ce8221c6fd795520aebcd7c353d8
Per Lejontand <per.lejontand@gmail.com>
2021-12-03 18:32:58 +0100

    Minor cleanup

--- 2021/python/dec01.py
+++ 2021/python/dec01.py
@@ -6,10 +6,8 @@ class dec01_1(task.IntTask):
     def run_list(self, data):
         incr = 0
         for (idx, d) in enumerate(data):
-            if idx > 0:
-                if data[idx -1] < d:
-
-                    incr += 1
+            if idx > 0 and data[idx -1] < d:
+                incr += 1
 
         return incr
 
@@ -17,7 +15,7 @@ class dec01_2(task.IntTask):
     def run_list(self, data):
         incr = 0
         for i in range(0, len(data) - 3):
-            if data[i] + data[i + 1] + data[i + 2] < data[i + 1] + data[i + 2] + data[i + 3]:
+            if sum(data[i:i+3]) < sum(data[i + 1:i + 4]):
                 incr += 1
 
         return incr
